4213d14d4e822e59390b3a63c807a8b3493089de
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2022-12-07 13:22:58 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,5 @@
-// Arrays of options for password to be generated from
-const lowercaseOptions = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "q",
-  "x",
-  "y",
-  "z",
-];
-const uppercaseOptions = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-const numericalOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const specialCharacterOptions = ["!", "@", "#", "$", "%", "^", "&", "*"];
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Get random element from an array function
 function getRandomElement(array) {
@@ -66,33 +9,112 @@ function getRandomElement(array) {
 
 // Generate password function
 function generatePassword() {
+  // Options for password to be generated from
+  const lowercaseOptions = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "q",
+    "x",
+    "y",
+    "z",
+  ];
+  const uppercaseOptions = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const numericalOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const specialCharacterOptions = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  // Where generated password will be stored
+  let userPassword = [];
   // Ask user for password length
-  const userChoiceLength = prompt(
+  let userChoiceLength = prompt(
     "How long would you like your password to be? (8–128 characters) (Numbers only)"
   );
-  if (!userChoiceLength) {
+  // Validate user choice
+  userChoiceLength = parseInt(userChoiceLength);
+  if (userChoiceLength >= 8 && userChoiceLength <= 128) {
+  } else {
     return;
   }
   // Ask user if password should contain lowercase letters
-  const userChoiceLowercase = confirm(
+  let userChoiceLowercase = confirm(
     "Would you like to include lowercase letters? Click OK if yes."
   );
+  // Validate user choice
+  if (userChoiceLowercase) {
+    userPassword = userPassword.concat(lowercaseOptions);
+  }
   // Ask user if password should contain uppercase letters
-  const userChoiceUppercase = confirm(
+  let userChoiceUppercase = confirm(
     "Would you like to include uppercase letters? Click OK if yes."
   );
+  // Validate user choice
+  if (userChoiceUppercase) {
+    userPassword = userPassword.concat(uppercaseOptions);
+  }
   // Ask user if password should contain numbers
-  const userChoiceNumbers = confirm(
+  let userChoiceNumbers = confirm(
     "Would you like to include numbers? Click OK if yes."
   );
+  // Validate user choice
+  if (userChoiceNumbers) {
+    userPassword = userPassword.concat(numericalOptions);
+  }
   // Ask user if password should contain special characters
-  const userChoiceSpecialCharacters = confirm(
+  let userChoiceSpecialCharacters = confirm(
     "Would you like to include special characters? (Including !@#$%^&*) Click OK if yes."
   );
+  // Validate user choice
+  if (userChoiceSpecialCharacters) {
+    userPassword = userPassword.concat(specialCharacterOptions);
+  }
+  // Return generated
+  return userPassword.join("");
 }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
