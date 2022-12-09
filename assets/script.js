ea5c344ae4e1284da788417ecb7a61504e1f5148
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2022-12-09 14:03:12 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 "use strict";
 
-// Get references to the #generate element
+// Get references to the #generate element, which is the Generate Password button
 const generateBtn = document.querySelector("#generate");
 
-// Get random value from an array
+// Get generated password to the #password element, which is the textbox
+const passwordText = document.querySelector("#password");
+
+// Get random value from an array with this function
 function getRandomValue(array) {
   const randomValue = array[Math.floor(Math.random() * array.length)];
   return randomValue;
@@ -83,74 +86,86 @@ function generatePassword() {
   );
 
   // Validate user choice
-  // userChoiceLength = parseInt(userChoiceLength);
   if (
     userChoiceLength < 8 ||
     userChoiceLength > 128 ||
-    !/^[0-9]+$/.test(userChoiceLength)
+    isNaN(userChoiceLength)
   ) {
-    alert("You must enter a number between 8–128 characters.");
-    userChoiceLength = prompt(
-      "How long would you like your password to be? (8–128 characters) (Digits only)"
+    alert(
+      "You must enter a number between 8–128 characters. Please try again."
     );
+    location.reload();
   }
 
   // Ask user if password should contain lowercase letters
   let userChoiceLowercase = confirm(
-    "Would you like to include lowercase letters? Click OK if yes. Click cancel if no."
+    "Would you like to include lowercase letters? Click OK if yes. Click Cancel if no."
   );
 
-  // Validate user choice
+  // If user choses to include lowercase letters, add lowercase letters from the array to the user choices array
   if (userChoiceLowercase) {
     userChoices = userChoices.concat(lowercaseOptions);
   }
 
   // Ask user if password should contain uppercase letters
   let userChoiceUppercase = confirm(
-    "Would you like to include uppercase letters? Click OK if yes. Click cancel if no."
+    "Would you like to include uppercase letters? Click OK if yes. Click Cancel if no."
   );
 
-  // Validate user choice
+  // If user choses to include uppercase letters, add uppercase letters from the array to the user choices array
   if (userChoiceUppercase) {
     userChoices = userChoices.concat(uppercaseOptions);
   }
 
   // Ask user if password should contain numbers
   let userChoiceNumbers = confirm(
-    "Would you like to include numbers? Click OK if yes. Click cancel if no."
+    "Would you like to include numbers? Click OK if yes. Click Cancel if no."
   );
 
-  // Validate user choice
+  // If user choses to include numbers, add numbers from the array to the user choices array
   if (userChoiceNumbers) {
     userChoices = userChoices.concat(numericalOptions);
   }
 
   // Ask user if password should contain special characters
   let userChoiceSpecialCharacters = confirm(
-    "Would you like to include special characters? (!@#$%^&*) Click OK if yes. Click cancel if no."
+    "Would you like to include special characters? (!@#$%^&*) Click OK if yes. Click Cancel if no."
   );
 
-  // Validate user choice
+  // If user choses to include special characters, add special characters from the array to the user choices array
   if (userChoiceSpecialCharacters) {
     userChoices = userChoices.concat(specialCharacterOptions);
   }
 
-  // Take user's password length choice and loop over the concated array
+  // Validate user choice
+  if (
+    !userChoiceLowercase &&
+    !userChoiceUppercase &&
+    !userChoiceNumbers &&
+    !userChoiceSpecialCharacters
+  ) {
+    alert(
+      "You must choose at least one set of critera for your password, either lowercase letters, uppercase letters, numbers, or special characters. Please try again."
+    );
+    location.reload();
+  }
+
+  // Take user's password length choice and loop it over the concated user choices array
   for (let i = 0; i < userChoiceLength; i++) {
     userPassword.push(getRandomValue(userChoices));
   }
 
-  // Return generated password
+  // Join and return the generated password array
   return userPassword.join("");
 }
 
-// Write password to the #password input
+// Write the password to the #password input
 function writePassword() {
   const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+  // const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Add event listener to Generate Password button
 generateBtn.addEventListener("click", writePassword);
